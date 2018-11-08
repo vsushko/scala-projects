11871b5eeb05665f7de0f6af12f2036b84e99863
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-11-09 00:46:45 +0300

    completed mkdir command and added ls command

---
+++ filesystem/src/main/scala/com/commands/Ls.scala
@@ -0,0 +1,25 @@
+package com.commands
+
+import com.files.DirEntry
+import com.filesystem.State
+
+/**
+  *
+  * @author vsushko
+  */
+class Ls extends Command {
+
+  override def apply(state: State): State = {
+    val contents = state.wd.contents
+    val niceOutput = createNiceOutput(contents)
+    state.setMessage(niceOutput)
+  }
+
+  def createNiceOutput(contents: List[DirEntry]): String = {
+    if (contents.isEmpty) ""
+    else {
+      val entry = contents.head
+      entry.name + "[" + entry.getType + "]\n" + createNiceOutput(contents.tail)
+    }
+  }
+}
