e826d92d8c3ad4b69125f40059cee9e74a53ce61
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-11-10 01:25:24 +0300

    added relative paths treatment

--- filesystem/src/main/scala/com/commands/Cd.scala
+++ filesystem/src/main/scala/com/commands/Cd.scala
@@ -43,11 +43,25 @@ class Cd(dir: String) extends Command {
         else findEntryHelper(nextDir.asDirectory, path.tail)
       }
 
+    @tailrec
+    def collapseRelativeTokens(path: List[String], result: List[String]): List[String] = {
+      if (path.isEmpty) result
+      else if (".".equals(path.head)) collapseRelativeTokens(path.tail, result)
+      else if ("..".equals(path.head)) {
+        if (result.isEmpty) null
+        else collapseRelativeTokens(path.tail, result.init)
+      } else collapseRelativeTokens(path.tail, result :+ path.head)
+    }
+
     // 1. tokens
     val tokens: List[String] = path.substring(1).split(Directory.SEPARATOR).toList
 
+    // 1.5 eliminate/collapse relative tokens
+    val newTokens = collapseRelativeTokens(tokens, List())
+
     // 2. navigate to the correct entry
-    findEntryHelper(root, tokens)
+    if (newTokens == null) null
+    else findEntryHelper(root, newTokens)
   }
 
 }
