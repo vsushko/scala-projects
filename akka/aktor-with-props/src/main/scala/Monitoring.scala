3161233f9c8aefacb192dfb95a8328651d10ec83
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-11-15 02:12:19 +0300

    supervision and monitoring

--- akka/aktor-with-props/src/main/scala/Monitoring.scala
+++ akka/aktor-with-props/src/main/scala/Monitoring.scala
@@ -1,7 +1,44 @@
+import akka.actor.{Actor, ActorRef, ActorSystem, Props, Terminated}
+
 /**
   *
   * @author vsushko
   */
-class Monitoring {
+class Ares(athena: ActorRef) extends Actor {
 
+  override def preStart(): Unit = {
+    context.watch(athena)
+  }
+
+  override def postStop(): Unit = {
+    println("Ares postStop...")
+  }
+
+  override def receive: Receive = {
+    case Terminated(_) =>
+      context.stop(self)
+  }
+}
+
+class Athena extends Actor {
+
+  override def receive: Receive = {
+    case msg => println(s"Athena received ${msg}")
+      context.stop(self)
+  }
+}
+
+object Monitoring extends App {
+  // create the 'monitoring' actor system
+  val system = ActorSystem("monitoring")
+
+  val athena = system.actorOf(Props[Athena], "athena")
+
+  val ares = system.actorOf(Props(classOf[Ares], athena), "ares")
+
+  athena ! "Hi"
+
+  Thread.sleep(500)
+
+  system.terminate()
 }
--- akka/aktor-with-props/src/main/scala/Supervision.scala
+++ akka/aktor-with-props/src/main/scala/Supervision.scala
@@ -1,7 +1,104 @@
+import Aphrodite.{RestartException, ResumeException, StopException}
+import akka.actor.SupervisorStrategy._
+import akka.actor.{Actor, ActorRef, ActorSystem, OneForOneStrategy, Props}
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
 /**
   *
   * @author vsushko
   */
-class Supervision {
+class Aphrodite extends Actor {
+
+  def receive = {
+    case "Resume" =>
+      throw ResumeException
+    case "Stop" =>
+      throw StopException
+    case "Restart" =>
+      throw RestartException
+    case _ =>
+      throw new Exception
+  }
+
+  override def preStart(): Unit = {
+    println("Aphrodite preStart hook...")
+  }
+
+  override def postStop(): Unit = {
+    println("Aphrodite postStop...")
+  }
+
+  override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+    println("Aphrodite postRestart hook...")
+    super.preRestart(reason, message)
+  }
+
+  override def postRestart(reason: Throwable): Unit = {
+    println("Aphrodite postRestart hook...")
+    super.postRestart(reason)
+  }
+}
+
+object Aphrodite {
+
+  case object ResumeException extends Exception
+
+  case object StopException extends Exception
+
+  case object RestartException extends Exception
+
+}
+
+class Hera extends Actor {
+
+  import Aphrodite._
+
+  var childRef: ActorRef = _
+
+  override val supervisorStrategy =
+    OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1 second) {
+      case ResumeException => Resume
+      case StopException => Restart
+      case RestartException => Stop
+      case _: Exception => Escalate
+    }
+
+  override def preStart(): Unit = {
+    // create Aphrodite Actor
+    childRef = context.actorOf(Props[Aphrodite], "Aphrodite")
+    Thread.sleep(100)
+  }
+
+  override def receive: Receive = {
+    case msg =>
+      println(s"Hera received ${msg}")
+      childRef ! msg
+      Thread.sleep(100)
+  }
+}
+
+object Supervision extends App {
+
+  // create the 'supervision' actor system
+  val system = ActorSystem("supervision")
+
+  // create Hera Actor
+  val hera = system.actorOf(Props[Hera], "hera")
+
+//  hera ! "Resume"
+//  Thread.sleep(1000)
+//  println()
+
+//    hera ! "Restart"
+//    Thread.sleep(1000)
+//    println()
+
+    hera ! "Stop"
+    Thread.sleep(1000)
+    println()
+
+  system.terminate()
 
 }
