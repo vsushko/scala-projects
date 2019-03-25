96674b9e836cfb29fbe85f11f3982ce4387dbd3a
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2019-03-25 21:27:14 +0300

    added set

--- rock-the-jvm-scala-advanced/src/main/scala/lectures/part2afp/MySet.scala
+++ rock-the-jvm-scala-advanced/src/main/scala/lectures/part2afp/MySet.scala
@@ -1,9 +1,106 @@
 package lectures.part2afp
 
+import scala.annotation.tailrec
+
 /**
+  * functional set
   *
   * @author vsushko
   */
-trait MySet {
+trait MySet[A] extends (A => Boolean) {
 
+  def contains(elem: A): Boolean
+
+  def +(elem: A): MySet[A]
+
+  def ++(anotherSet: MySet[A]): MySet[A]
+
+  def map[B](f: A => B): MySet[B]
+
+  def flatMap[B](f: A => MySet[B]): MySet[B]
+
+  def filter(predicate: A => Boolean): MySet[A]
+
+  def foreach(f: A => Unit): Unit
+
+  def apply(elem: A): Boolean = contains(elem)
+}
+
+class EmptySet[A] extends MySet[A] {
+  override def contains(elem: A): Boolean = false
+
+  override def +(elem: A): MySet[A] = new NonEmptySet[A](elem, this)
+
+  override def ++(anotherSet: MySet[A]): MySet[A] = anotherSet
+
+  override def map[B](f: A => B): MySet[B] = new EmptySet[B]
+
+  override def flatMap[B](f: A => MySet[B]): MySet[B] = new EmptySet[B]
+
+  override def filter(predicate: A => Boolean): MySet[A] = this
+
+  override def foreach(f: A => Unit): Unit = ()
+}
+
+class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
+  override def contains(elem: A): Boolean =
+    elem == head || tail.contains(elem)
+
+  override def +(elem: A): MySet[A] =
+    if (this contains elem) this
+    else new NonEmptySet[A](elem, this)
+
+  // [1 2 3] ++ [4 5]
+  // [2 3] ++ [4 5] + 1
+  // [3] ++ [4 5] + 1 + 2
+  // [] ++ [4 5] + 1 + 2 + 3
+  // [4 5] + 1 + 2 + 3 = [4 5 1 2 3]
+  override def ++(anotherSet: MySet[A]): MySet[A] = tail ++ anotherSet + head
+
+  override def map[B](f: A => B): MySet[B] = (tail map f) + f(head)
+
+  override def flatMap[B](f: A => MySet[B]): MySet[B] = (tail flatMap f) ++ f(head)
+
+  override def filter(predicate: A => Boolean): MySet[A] = {
+    val filteredTail = tail filter predicate
+    if (predicate(head)) {
+      filteredTail + head
+    } else {
+      filteredTail
+    }
+  }
+
+  override def foreach(f: A => Unit): Unit = {
+    f(head)
+    tail foreach f
+  }
+}
+
+object MySet {
+  /*
+    val s = MySet(1,2,3) = buildSet(seq(1,2,3), [])
+    = buildSet(seq(2,3), [] + 1)
+    = buildSet(seq(3), [1] + 2)
+    = buildSet(seq(), [1,2] + 3)
+    = [1,2,3]
+   */
+
+  def apply[A](values: A*): MySet[A] = {
+    @tailrec
+    def buildSet(valSeq: Seq[A], acc: MySet[A]): MySet[A] =
+      if (valSeq.isEmpty) acc
+      else buildSet(valSeq.tail, acc + valSeq.head)
+
+    buildSet(values.toSeq, new EmptySet[A])
+  }
+}
+
+object MySetPlayground extends App {
+  val s = MySet(1, 2, 3, 4)
+  // s + 5 foreach println
+  // s + 5 ++ MySet(-1, -2) foreach println
+  // s + 5 ++ MySet(-1, -2) + 3 foreach println
+  // s + 5 ++ MySet(-1, -2) + 3 map (x => x * 10) foreach println
+  // s + 5 ++ MySet(-1, -2) + 3 flatMap (x => MySet(x, 10 * x)) foreach println
+  s + 5 ++ MySet(-1, -2) + 3 flatMap (x => MySet(x, 10 * x)) filter (_ % 2 == 0) foreach println
 }
