563a56fee55327967e8b8ecd965618fcc60768ed
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2019-07-24 22:57:01 +0300

    thread communications

--- rock-the-jvm-scala-advanced/src/main/scala/lectures/part3concurrency/ThreadCommunication.scala
+++ rock-the-jvm-scala-advanced/src/main/scala/lectures/part3concurrency/ThreadCommunication.scala
@@ -1,5 +1,8 @@
 package lectures.part3concurrency
 
+import scala.collection.mutable
+import scala.util.Random
+
 /**
   *
   * @author vsushko
@@ -77,6 +80,59 @@ object ThreadCommunication extends App {
     producer.start()
   }
 
-  smartProdCons()
+  // smartProdCons()
+
+  def prodConsLargeBuffer(): Unit = {
+    val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
+    val capacity = 3
+
+    val consumer = new Thread(() => {
+      val random = new Random()
+
+      while (true) {
+        buffer.synchronized {
+          if (buffer.isEmpty) {
+            println("[consumer] buffer empty, waitin...")
+            buffer.wait()
+          }
+          // there must be at least ONE value in the buffer
+          val x = buffer.dequeue()
+          println("[consumer] consumed " + x)
+
+          // hey producer, there's empty space available, are you lazy?)
+          buffer.notify()
+        }
+        Thread.sleep(random.nextInt(500))
+      }
+    })
+
+    val producer = new Thread(() => {
+      val random = new Random()
+      var i = 0
+
+      while (true) {
+        buffer.synchronized {
+          if (buffer.size == capacity) {
+            println("[producer] buffer is full, waiting... ")
+            buffer.wait()
+          }
+
+          // there must be a least ONE EMPTY SPACE in the buffer
+          println("[producer] producing " + i)
+          buffer.enqueue(i)
+
+          // hey consumer, new food for you
+          buffer.notify()
+
+          i += 1
+        }
+        Thread.sleep(random.nextInt(500))
+      }
+    })
+    consumer.start()
+    producer.start()
+  }
+
+  prodConsLargeBuffer()
 
 }
