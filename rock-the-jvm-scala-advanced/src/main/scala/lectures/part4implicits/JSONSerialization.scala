ab5ca01a246937ad1000a137b12d04fe1f4af3a6
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2019-08-03 17:08:50 +0300

    type classes: json serialization

---
+++ rock-the-jvm-scala-advanced/src/main/scala/lectures/part4implicits/JSONSerialization.scala
@@ -0,0 +1,122 @@
+package lectures.part4implicits
+
+import java.util.Date
+
+/**
+  *
+  * @author vsushko
+  */
+object JSONSerialization extends App {
+
+  /*
+      Users, posts, feeds
+      Serialize to JSON
+   */
+
+  case class User(name: String, age: Int, email: String)
+
+  case class Post(content: String, createdAt: Date)
+
+  case class Feed(user: User, posts: List[Post])
+
+  /*
+      1 - intermediate data types Int, String, List, Date
+      2 - type classes for conversion to intermediate data types
+      3 - serialize to JSON
+   */
+
+  // intermediate data type
+  sealed trait JSONValue {
+    def stringify: String
+  }
+
+  final case class JSONString(value: String) extends JSONValue {
+    override def stringify: String =
+      "\"" + value + "\""
+  }
+
+  final case class JSONNumber(value: Int) extends JSONValue {
+    override def stringify: String = value.toString
+  }
+
+  final case class JSONArray(values: List[JSONValue]) extends JSONValue {
+    override def stringify: String = values.map(_.stringify).mkString("[", ",", "]")
+  }
+
+  final case class JSONObject(values: Map[String, JSONValue]) extends JSONValue {
+    override def stringify: String = values.map {
+      case (key, value) => "\"" + key + "\":" + value.stringify
+    }.mkString("{", ",", "}")
+  }
+
+  val data = JSONObject(Map(
+    "user" -> JSONString("Daniel"),
+    "posts" -> JSONArray(List(
+      JSONString(" Scala Rocks!"),
+      JSONNumber(453)
+    ))
+  ))
+
+  println(data.stringify)
+
+  // type class
+  /*
+    1 - type class
+    2 - type class instances (implicit)
+    3 - pimp library to use type class instances
+   */
+
+  // 2.1
+  trait JSONConverter[T] {
+    def convert(value: T): JSONValue
+  }
+
+  // 2.3 conversion
+  implicit class JSONOps[T](value: T) {
+    def toJSON(implicit converter: JSONConverter[T]): JSONValue =
+      converter.convert(value)
+  }
+
+  // 2.2
+  implicit object StringConverter extends JSONConverter[String] {
+    def convert(value: String): JSONValue = JSONString(value)
+  }
+
+  implicit object NumberConverter extends JSONConverter[Int] {
+    def convert(value: Int): JSONValue = JSONNumber(value)
+  }
+
+  // custom data types
+  implicit object UserConverter extends JSONConverter[User] {
+    def convert(user: User): JSONValue = JSONObject(Map(
+      "name" -> JSONString(user.name),
+      "age" -> JSONNumber(user.age),
+      "email" -> JSONString(user.email)
+    ))
+  }
+
+  implicit object PostConverter extends JSONConverter[Post] {
+    def convert(post: Post): JSONValue = JSONObject(Map(
+      "content" -> JSONString(post.content),
+      "created" -> JSONString(post.createdAt.toString)
+    ))
+  }
+
+  implicit object FeedConverter extends JSONConverter[Feed] {
+    def convert(feed: Feed): JSONValue = JSONObject(Map(
+      "user" -> feed.user.toJSON,
+      "posts" -> JSONArray(feed.posts.map(_.toJSON))
+    ))
+  }
+
+  // call stringify on result
+  val now = new Date(System.currentTimeMillis())
+  val john = User("John", 34, "john@rockthejvm.com")
+  val feed = Feed(john, List(
+    Post("hello", now),
+    Post("look at this cute puppy", now)
+  ))
+
+  println(feed.toJSON.stringify)
+
+}
