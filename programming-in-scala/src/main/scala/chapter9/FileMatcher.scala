a56134ea312052eeb30a12210eefbadfd12b55c6
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2019-11-25 11:44:04 +0300

    chapter 9

--- programming-in-scala/src/main/scala/chapter9/FileMatcher.scala
+++ programming-in-scala/src/main/scala/chapter9/FileMatcher.scala
@@ -7,6 +7,17 @@ package chapter9
 object FileMatcher {
   private def filesHere = (new java.io.File(".")).listFiles
 
-  def filesEnding(query: String) = for (file <- filesHere; if file.getName.endsWith(query))
-    yield file
+  def filesMatching(query: String, matcher: (String, String) => Boolean) = {
+    for (file <- filesHere; if matcher(file.getName, query))
+      yield file
+  }
+
+  def filesEnding(query: String) =
+    filesMatching(query, _.endsWith(_))
+
+  def filesContaining(query: String) =
+    filesMatching(query, _.contains(_))
+
+  def filesRegex(query: String) =
+    filesMatching(query, _.matches(_))
 }
