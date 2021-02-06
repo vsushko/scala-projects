e6076ba3289a744767693f47e08ff7dda78553e0
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2021-02-06 23:29:55 +0300

    added datasources

---
+++ big-data-essentials-with-scala/src/main/scala/part2dataframes/ColumnsAndExpressions.scala
@@ -0,0 +1,20 @@
+package part2dataframes
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  *
+  * @author vsushko
+  */
+object ColumnsAndExpressions extends App {
+
+  val spark = SparkSession.builder()
+    .appName("DF Columns and Expressions")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val carsDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/cars.json")
+
+}
