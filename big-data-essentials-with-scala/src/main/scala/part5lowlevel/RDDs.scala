9bb9dbe056cdc18cf9ec583fa6ccd324ca677ff2
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2021-03-03 02:01:05 +0300

    rdds and exercise

--- big-data-essentials-with-scala/src/main/scala/part5lowlevel/RDDs.scala
+++ big-data-essentials-with-scala/src/main/scala/part5lowlevel/RDDs.scala
@@ -1,6 +1,7 @@
 package part5lowlevel
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{SaveMode, SparkSession}
 
 import scala.io.Source
 
@@ -22,7 +23,7 @@ object RDDs extends App {
   val numbersRDD = sc.parallelize(numbers)
 
   // 2 - reading from files
-  case class StockValue(company: String, date: String, price: Double)
+  case class StockValue(symbol: String, date: String, price: Double)
 
   def readStocks(filedName: String) =
     Source.fromFile(filedName)
@@ -56,5 +57,124 @@ object RDDs extends App {
 
   // RDD -> DS
   val numbersDS = spark.createDataset(numbersRDD) // you get to keep type info
+
+  // transformations
+  // distinct
+  val msftRDD = stocksRDD.filter(_.symbol == "MSFT") // lazy transformation
+  val msCount = msftRDD.count() // eager ACTION
+
+  // counting
+  val companyNamesRDD = stocksRDD.map(_.symbol).distinct() // also lazy
+
+  // min and max
+  implicit val stockOrdering: Ordering[StockValue] =
+    Ordering.fromLessThan((sa: StockValue, sb: StockValue) => sa.price < sb.price)
+
+  val minMsft = msftRDD.min() // action
+
+  // reduce
+  numbersRDD.reduce(_ + _)
+
+  // grouping
+  val groupedStockRDD = stocksRDD.groupBy(_.symbol)
+  // ^^ very expensive
+
+  // Partitioning
+  val repartitionedStocksRDD = stocksRDD.repartition(30)
+  repartitionedStocksRDD.toDF.write
+    .mode(SaveMode.Overwrite)
+    .parquet("src/main/resources/data/stocks30")
+
+  /*
+    Repartitioning is EXPENSIVE. Involves Shuffling
+    Best practice: partition EARLY, then process that.
+    Size of a partition should be 10-100MB.
+   */
+
+  // coalesce
+  val coalescedRDD = repartitionedStocksRDD.coalesce(15) // does NOT involve shuffling
+  coalescedRDD.toDF.write
+    .mode(SaveMode.Overwrite)
+    .parquet("src/main/resources/data/stocks15")
+
+  /**
+    * Exercises
+    *
+    * 1. Read the movies.json as an RDD.
+    * 2. Show the distinct genres as an RDD.
+    * 3. Select all the movies in the Drama genre with IMDB rating > 6.
+    * 4. Show the average rating of movies by genre.
+    */
+
+  case class Movie(title: String, genre: String, rating: Double)
+
+  // 1
+  val moviesDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/movies.json")
+
+  val moviesRDD = moviesDF
+    .select(col("Title").as("title"), col("Major_Genre").as("genre"), col("IMDB_Rating").as("rating"))
+    .where(col("genre").isNotNull and col("rating").isNotNull)
+    .as[Movie]
+    .rdd
+
+  // 2
+  val genresRDD = moviesRDD.map(_.genre).distinct()
+
+  // 3
+  val goodDramasRDD = moviesRDD.filter(movie => movie.genre == "Drama" && movie.rating > 6)
+
+  moviesRDD.toDF.show()
+  genresRDD.toDF.show()
+  goodDramasRDD.toDF.show()
+
+  // 4
+  case class GenreAvgRating(genre: String, rating: Double)
+
+  val avgRatingByGenreeRDD = moviesRDD.groupBy(_.genre).map {
+    case (genre, movies) =>
+      GenreAvgRating(genre, movies.map(_.rating).sum / movies.size)
+  }
+
+  avgRatingByGenreeRDD.toDF.show()
+  moviesRDD.toDF.groupBy(col("genre")).avg("rating").show()
+
+  /*
+    Reference
+    +-------------------+------------------+
+    |              genre|            rating|
+    +-------------------+------------------+
+    |Concert/Performance|             6.325|
+    |            Western| 6.842857142857142|
+    |            Musical|             6.448|
+    |             Horror|5.6760765550239185|
+    |    Romantic Comedy| 5.873076923076922|
+    |             Comedy| 5.853858267716529|
+    |       Black Comedy|6.8187500000000005|
+    |        Documentary| 6.997297297297298|
+    |          Adventure| 6.345019920318729|
+    |              Drama| 6.773441734417339|
+    |  Thriller/Suspense| 6.360944206008582|
+    |             Action| 6.114795918367349|
+    +-------------------+------------------+
+
+    +-------------------+------------------+
+    |              genre|       avg(rating)|
+    +-------------------+------------------+
+    |          Adventure| 6.345019920318729|
+    |              Drama| 6.773441734417339|
+    |        Documentary| 6.997297297297298|
+    |       Black Comedy|6.8187500000000005|
+    |  Thriller/Suspense| 6.360944206008582|
+    |            Musical|             6.448|
+    |    Romantic Comedy| 5.873076923076922|
+    |Concert/Performance|             6.325|
+    |             Horror|5.6760765550239185|
+    |            Western| 6.842857142857142|
+    |             Comedy| 5.853858267716529|
+    |             Action| 6.114795918367349|
+    +-------------------+------------------+
+   */
 
 }
